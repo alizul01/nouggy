939c2347a1da5bce76d9020da13f5515ed35fa45
alizul01 <alizulfikar032@gmail.com>
2023-06-29 21:03:11 +0700

    update seo site metadata

--- config/site.ts
+++ config/site.ts
@@ -3,7 +3,7 @@ export type SiteConfig = typeof siteConfig
 export const siteConfig = {
   name: "Nouggy",
   description:
-    "Nouggy adalah",
+    "Nouggy adalah inovasi nougat millenial yang terbuat dari kulit ari kedelai dan ramah lingkungan. Dibuat dengan cinta di Malang, Indonesia, Nouggy menghadirkan manis yang sehat untuk memanjakan lidahmu. Rasakan kelezatan unik dan kualitas premium dari nougat kami, yang dijamin membuatmu ketagihan. Nikmati sensasi unik nouggy yang menggoda dan jadikan pilihan sehat untuk kebahagiaanmu. Fit Nouggy For Your Health! 🍬💚",
   links: {
     instagram: "https://www.instagram.com/nouggy.co"
   },
